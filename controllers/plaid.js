b5525dc36924c9825ba378dddf199a6da0c43bd2
Asim Zaidi <asimzaidih@gmail.com>
2019-07-19 00:57:58 -0700

    accounts log

--- controllers/plaid.js
+++ controllers/plaid.js
@@ -77,15 +77,16 @@ const transactions = async (request, response, next) => {
       });
     } else { // Success
       console.log('res.transactions:', res.transactions);
+      console.log('res.transactions:', res.transactions.accounts);
       
       // console.log('res.transactions.accounts[0]:', res.transactions.accounts[0]);
-      // const currentBalance = res.transactions.accounts[0].available;
-      // // let balance = new Balance({date: req.body.today, value: currentBalance});
+      // const currentBalance = res.transactions.accounts[0].balances.available;
+      // let balance = new Balance({date: req.body.today, value: currentBalance});
       // let balance = new Balance({value: currentBalance});
       // balance.save();
       // user.balances.push(balance);
       // user.transactions = req.transactions.transactions;
-      // user.save();
+      user.save();
       response.json({error: null, user});
     }
   });
