a40d09920d252feac7d0c7a15e271dafd23d54c7
Asim Zaidi <asimzaidih@gmail.com>
2019-09-01 20:19:01 -0500

    notification logic finished

--- controllers/notification.js
+++ controllers/notification.js
@@ -7,7 +7,6 @@ const overdraftNotification = async (req, res) => {
         let user = await User.findById(req.body.userId);
         user.overdraftNotification = req.body.notificationOnOrOff;
         user.save();
-        // I've never sent back specific data like this to on the client update a property on an object in my store!
         return res.json(user.overdraftNotification);
     } catch(e) {
         return res.json('Something went wrong, please try again or contact support.');
@@ -20,7 +19,7 @@ const minimumBalanceNotification = async (req, res) => {
         req.user.minimumBalanceNotification = req.body.minimumBalanceNotification;
         req.user.minimumBalanceAmount = req.body.minimumBalanceAmount;
         user.save();
-        return res.json(user);
+        return res.json(user.minimumBalanceAmount);
     } catch(e) {
         return res.json('Something went wrong, please try again or contact support.');
     }
@@ -29,10 +28,10 @@ const minimumBalanceNotification = async (req, res) => {
 const bigTransactionNotification = async (req, res) => {
     try {
         let user = await User.findById(req.body.userId);
-        req.user.bigTransactionNotification = req.body.bigTransactionNotification;
-        req.user.bigTransactionAmount = req.body.bigTransactionAmount;
+        user.bigTransactionNotification = req.body.bigTransactionNotification;
+        user.bigTransactionAmount = req.body.bigTransactionAmount;
         user.save();
-        return res.json(user);
+        return res.json(user.bigTransactionAmount);
     } catch(e) {
         return res.json('Something went wrong, please try again or contact support.');
     }
