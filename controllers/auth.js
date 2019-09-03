d736eea9a5f630e247785342afc507b622afed82
Ikey Benzaken <ikey.benz@gmail.com>
2019-09-03 15:27:06 -0700

    [linting] code cleanup and also removed the lodash require

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,52 +1,51 @@
 
-const _ = require('lodash');
-const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const User = require('../models/user');
 
-const signup = async (req, res) => {  
+const signup = async (req, res) => {
   const data = req.body;
   try {
-    const email = data.email;
-    let user = await User.findOne({email}, "email notificationToken");
+    const { email } = data;
+    const user = await User.findOne({ email }, 'email notificationToken');
     if (user) {
       res.status(401).send('Account with this email already exists');
-    };
-    
-    const newUser = new User(data); 
+    }
+
+    const newUser = new User(data);
     await newUser.save();
-    const token = jwt.sign({ _id: newUser._id }, process.env.SECRET, { expiresIn: "60 days" });   
+    const token = jwt.sign({ _id: newUser._id }, process.env.SECRET, { expiresIn: '60 days' });
     res.cookie('csToken', token, { maxAge: 900000, httpOnly: false });
     return res.status(200).json(newUser);
-  } catch(err) {
-    res.status(401).send(err);
+  } catch (err) {
+    return res.status(401).send(err);
   }
-}
+};
 
 const login = async (req, res) => {
   const data = req.body;
 
   try {
     const { email, password } = data;
-    let user = await User.findOne({email}, "email password public_key");
+    const user = await User.findOne({ email }, 'email password public_key');
     if (!user) {
-      res.status(401).send('Wrong Email');
-    };
+      return res.status(401).send('Wrong Email');
+    }
 
     user.comparePassword(password, (err, isMatch) => {
-        if (!isMatch) {
-          res.status(401).send('Wrong Email or Password');
-        } else {
-          const token = jwt.sign({_id: user._id, email: user.email}, process.env.SECRET, { expiresIn: "60 days" });
-          res.cookie("csToken", token, {maxAge: 900000, httpOnly: false});
-          return res.status(200).send(user);  
-        }
+      if (!isMatch) {
+        res.status(401).send('Wrong Email or Password');
+      } else {
+        const token = jwt.sign({ _id: user._id, email: user.email }, process.env.SECRET, { expiresIn: '60 days' });
+        res.cookie('csToken', token, { maxAge: 900000, httpOnly: false });
+        return res.status(200).send(user);
+      }
     });
   } catch (err) {
-    res.status(500).send(err);
+    return res.status(500).send(err);
   }
 };
 
 module.exports = {
   signup,
-  login
-}
+  login,
+};
